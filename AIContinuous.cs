aa91ab252861bae962e7faca2d91d34893025061
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-02-19 16:27:05 -0300

    False Position method

--- AIContinuous.cs
+++ AIContinuous.cs
@@ -2,19 +2,25 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Threading.Tasks.Dataflow;
 
 namespace IA
 {
     public class Root
     {
         private static double x0 { get; set; }
+        private static double x1 { get; set; }
         private static double x2 { get; set; }
+        private static double y0 { get; set; }
+        private static double y1 { get; set; }
         private static double middle { get; set; }
+        public static double bis { get; set; }
 
-        public static double Bisection(Func<double, double> function, double a, double b, double tol = 1e-4, int maxIter = 1000)
+        public static double Bisection(Func<double, double> function, double a, double b, double aTol = 1e-4, double rTol = 1e-4, int maxIter = 1000)
         {
             x2 = 0;
-            
+
             for (int i = 0; i < maxIter; i++)
             {
                 middle = (a + b) / 2;
@@ -25,9 +31,30 @@ namespace IA
                     a = middle;
                 else
                     b = middle;
+
+                if (Math.Abs(x2) < aTol)
+                    break;
+
+                if (x2 - x0 < 2.0 * rTol)
+                    break;
             }
 
-            return (a + b) / 2;
+            return middle;
+        }
+
+        public static double FalsePosition(Func<double, double> function, double a, double b, double aTol = 1e-4, double rTol = 1e-4, int maxIter = 1000)
+        {
+            x0 = a;
+            x1 = b;
+
+            y0 = function(x0);
+            y1 = function(x1);
+
+            x2 = (-y0) * ((x1 - x0) / (y1 - y0)) + x0;
+
+            bis = Bisection(function, x2, x1);
+
+            return bis;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ double MyFunction(double x)
     return x + 1;
 }
 
-var sol = Root.Bisection(MyFunction, -10, 10);
+var sol = Root.FalsePosition(MyFunction, -10, 10);
 Console.WriteLine(sol);
