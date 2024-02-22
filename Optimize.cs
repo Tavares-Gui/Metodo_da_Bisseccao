e64824deb8f1895f4f26612d2cb69166284496e0
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-02-22 14:27:40 -0300

    Optimized Gradient Descendent

--- Optimize.cs
+++ Optimize.cs
@@ -3,7 +3,7 @@ namespace IA;
 public static class Optimize
 {
     public static double Newton(
-        Func<double, double> function, double x0, 
+        Func<double, double> function, double x0,
         double h = 1e-2, double aTol = 1e-4, int maxIter = 10000
     )
     {
@@ -14,18 +14,45 @@ public static class Optimize
     }
 
     public static double GradientDescent(
-        Func<double, double> function, double x0, 
+        Func<double, double> function, double x0,
         double lr = 1e-2, double aTol = 1e-4
     )
     {
         double xp = x0;
         double diff = Diff.Differentiate(function, xp);
-        
-        while(Math.Abs(diff) > aTol)
+
+        while (Math.Abs(diff) > aTol)
         {
             diff = Diff.Differentiate(function, xp);
             xp -= lr * diff;
         }
+
+        return xp;
+    }
+
+    public static double[] GradientDescent(
+        Func<double[], double> function, double[] x0,
+        double lr = 1e-2, double aTol = 1e-4
+    )
+    {
+        var dim = x0.Length;
+        var xp = (double[])x0.Clone();
+        double[] diff = Diff.Gradient(function, xp);
+        double diffNorm;
+
+        do
+        {
+            diffNorm = 0.0;
+            diff = Diff.Gradient(function, xp);
+
+            for (int i = 0; i < dim; i++)
+            {
+                xp[i] -= lr * diff[i];
+                diffNorm += Math.Abs(diff[i]);
+            }
+
+            diffNorm /= dim;
+        } while (diffNorm > dim * aTol);
 
         return xp;
     }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿using IA;
+﻿using System.Runtime.CompilerServices;
+using IA;
 
 double MyFunction(double x)
 {
     return (x - 1) * (x - 1) + Math.Sin(x * x * x);
+}
+
+double MyFunctionDims(double[] x)
+{
+    return x[0] * x[0] + x[1] * x[1];
+    // return (x[0] + 2 * x[1] - 7) * (x[0] + 2 * x[1] - 7) + (2 * x[0] + x[1] - 5) * (2 * x[0] + x[1] - 5);
 }
 
 // double MyDer(double x)
@@ -11,35 +18,36 @@ double MyFunction(double x)
 // }
 
 double sol;
+double[] solVal;
 var date = DateTime.Now;
-double chute = 1.0;
 
 // date = DateTime.Now;
 // sol = Root.Bisection(MyFunction, -10.0, 10.0);
 // Console.WriteLine($"Solution: {sol} | Time: {(DateTime.Now - date).TotalMilliseconds}");
 
+
 // date = DateTime.Now;
 // sol = Root.FalsePosition(MyFunction, -10.0, 10.0);
 // Console.WriteLine($"Solution: {sol} | Time: {(DateTime.Now - date).TotalMilliseconds}");
+
 
 // date = DateTime.Now;
 // sol = Root.Newton(MyFunction, MyDer, 10.0);
 // Console.WriteLine($"Solution: {sol} | Time: {(DateTime.Now - date).TotalMilliseconds}");
 
+
 // date = DateTime.Now;
 // sol = Root.Newton(MyFunction, double(double x) => Diff.Differentiate(MyFunction, x), 10.0);
 // Console.WriteLine($"Solution: {sol} | Time: {(DateTime.Now - date).TotalMilliseconds}");
 
+
+// date = DateTime.Now;
+// sol = Optimize.Newton(MyFunction, 1);
+
+// Console.WriteLine($"Solution: {sol} | Time: {(DateTime.Now - date).TotalMilliseconds}");
+
+double[] val = {27, 8};
 date = DateTime.Now;
-sol = Optimize.Newton(MyFunction, chute);
+solVal = Optimize.GradientDescent(MyFunctionDims, val);
 
-// double oldSol = Optimize.Newton(MyFunction, chute + 1);
-
-// while (sol > oldSol)
-// {
-//     chute += 5;
-//     sol = Optimize.Newton(MyFunction, chute);
-//     oldSol = Optimize.Newton(MyFunction, chute);
-// }
-
-Console.WriteLine($"Solution: {sol} | Time: {(DateTime.Now - date).TotalMilliseconds}");
+Console.WriteLine($"x: {solVal[0]} | y: {solVal[1]} | Time: {(DateTime.Now - date).TotalMilliseconds}");
